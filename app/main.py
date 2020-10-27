6eea326e0af03df5d18b05eae29d8c907e9fd5f2
santiago Jimenez Raigosa <santiago.jimenez.raigosa@hotmail.com>
2020-10-26 20:50:25 -0500

    comentarios chidos

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app= Flask(__name__)
+app = Flask(__name__)
+""" entre los // y los : es el usuario """
+""" entre : y el @ es la contraseña """
+""" entre @ y / es el host o dominio """
+""" el resto asi bien vergas es el nombre de la base de datos """
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b8a58880bd5f0e:e8b40148@us-cdbr-east-02.cleardb.com/heroku_adffbf225414c15'
 db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-  return "<h1>Bienvenido</h1>"
+    return "<h1>Bienvenido</h1>"
+
+
+""" Modelo de la bd """
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,12 +25,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
+    """ Ejemplos """
+
+
 @app.route('/ejemplo', methods=['POST'])
 def insertar():
-    username=request.json['username']
-    email=request.json['email']
-    me = User(username, email)
+    username = request.json['username']
+    email = request.json['email']
+    me = User(username=username, email=email)
     db.session.add(me)
     db.session.commit()
     if(me.id):
@@ -30,13 +41,35 @@ def insertar():
     else:
         return jsonify({'message': 'Error'})
 
+
 @app.route('/ejemplo/<string:id>', methods=['DELETE'])
 def eliminar(id):
     db.session.delete(me)
     db.session.commit()
     return jsonify({'message': 'Elimado'})
 
+
 @app.route('/ejemplo', methods=['GET'])
 def obtenerTodos():
     return jsonify(User.query.all())
 
+
+""" RUTAS SANTIAGO JIMENEZ RAIGOSA """
+
+""" GET """
+""" Retorna los clientes del VaR """
+
+""" Retorna un cliente especifico que tenga el valor en riesgo """
+
+""" POST """
+""" Crea un nuevo cliente para el VaR """
+
+""" PUT """
+""" Actualiza un cliente para el VaR """
+
+""" Actualiza un cliente especifico para el VaR """
+
+""" DELETE """
+""" Borra un cliente para un VaR """
+
+""" Borra un cliente especifico para el VaR """
